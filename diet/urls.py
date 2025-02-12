d50bba8e24768f2dbbd29048b6353373e51d25f0
sailathakakumanu <166479434+sailathakakumanu@users.noreply.github.com>
2025-02-12 20:32:35 +0530

    Update urls.py

--- diet/urls.py
+++ diet/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # Landing page
     path('get-started/', views.get_started, name='get_started'),  # Get Started page
     path('meal-plan/', views.meal_plan, name='meal_plan'),  # Meal plan page
+    path('signup/',views.signup,name='signup'),
 ]
